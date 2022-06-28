93bc5e2947a72e7f30daec2dd6dff00b3547badb
Pranav C <pranavxc@gmail.com>
2022-06-28 13:33:37 +0530

    Update nocodb.rb

--- Formula/nocodb.rb
+++ Formula/nocodb.rb
@@ -1,10 +1,10 @@
 class Nocodb < Formula
-  desc "Get Human Readable file size information. - CLI"
+  desc "NocoDB"
   homepage "https://github.com/nocodb/nocodb"
-  url "https://github.com/pranavxc/nocodb/releases/download/0.91.7/nocodb.tar.gz"
-  sha256 "873892a33cc492aa2c68ff2aac42e4cbc3ef562ee46efd9d3e275faad2fa32d0"
+  url "https://github.com/nocodb/nocodb/releases/download/0.92.0/nocodb.tar.gz"
+  sha256 "919b7ffd43c3bce87b2a971744479e11d8047bb55cc75a3a1008fd7bf2b7de9b"
   license "MIT"
-  version "0.91.7"
+  version "0.92.0"
 
   def install
     bin.install "nocodb"
