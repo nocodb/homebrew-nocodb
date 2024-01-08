0ab022ea1e6a2bcc5a04a6b46caffbbc7634be39
Daniel Bayley <daniel.bayley@me.com>
2024-01-08 23:23:21 +0000

    Add `brew install --HEAD` option

--- Formula/nocodb.rb
+++ Formula/nocodb.rb
@@ -5,6 +5,7 @@ class Nocodb < Formula
   sha256 "a59af2a4dbaa4d8e005c33ddae31768321c781dec1e44424e9316d7020370e21"
   license "MIT"
   version "0.203.2"
+  head "#{homepage}.git", branch: "develop"
 
   def install
     bin.install "nocodb"
